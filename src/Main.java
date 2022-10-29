062bc24f7bae6011b9788c7a2ae7327ed824e0b5
Александр <alexakr1993@gmail.com>
2022-10-29 07:40:09 +0400

    Условный оператор else

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,39 @@ public class Main {
     public static void main(String[] args) {
         ///Task 1
         int age = 25;
-        if ( age >= 18 )
-        {
-            System.out.println("Поздравляем с совершеннолетием");
-        }
-        else
-        {
-            System.out.println("Возраст совершеннолетия еще не наступил, придется подождать");
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в детский сад");
+        } else if (age >= 7 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в школу");
+        } else if (age >= 18 && age <= 24) {
+            System.out.println("Если возраст человека равен " + age + " то его место в университете");
+        } else if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + " то ему пора ходить на работу");
         }
 
         ///Task 2
-        if (age >= 7 && age < 18 ) {
-            System.out.println("Ребенок идет в школу");
-        } else if (age >= 18 && age <= 24 ) {
-            System.out.println("Человек уже закончил школу и может отправляться в университет");
-        } else if  (age > 24 ) {
-            System.out.println("Человек уже окончил университет и ему пора искать первую работу");
+        int age1 = 7;
+        if (age1 < 5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        } else if (age1 < 14) {
+            System.out.println("Может кататься в сопровождении взрослого. Если взрослого нет, то кататься нельзя");
+        } else if ( age > 14 ) {
+            System.out.println("Может кататься без сопровождения взрослого");
         }
 
         ///Task 3
-        int people = 102;
-        int seats = 60;
-        int stand = people - seats;
-        int seatsUsed = 5;
-        int standUsed =  62;
-        if ( seatsUsed + standUsed > people ) {
-            System.out.println("Вагон забит");
-        }
-        if ( seats >= seatsUsed ) {
-            System.out.println("Есть еще " + (seats - seatsUsed) + " cидячих мест");
-        }
-        if ( stand >= standUsed) {
-            System.out.println( "Есть еще " + (stand - standUsed) + " стоячих мест");
-        }
-        else if ( seats < seatsUsed ) {
-            System.out.println("Сидячих мест нет");
-        }
-        else if ( stand < standUsed) {
-            System.out.println("Стоячих мест нет");
+        int one = 300;
+        int two = 150;
+        int three = 68;
+        if (one > two && one > three) {
+            System.out.println(one + " большее число.");
+        } else if (two > one && two > three ) {
+            System.out.println(two + " большее число");
+        } else if (three > one && three > two) {
+            System.out.println(three + " большее число.");
         }
 
 
 
-
-
-
-
-
-
+        }
     }
-}
